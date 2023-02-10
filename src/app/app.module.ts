2818b32911c13382ce22cef0f92025581efe91fe
Jack Pulsfort <pulsfortjf@appstate.edu>
2023-02-10 10:58:48 -0500

    finished combination lock component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CombinationLock } from './combination-lock/combination-lock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CombinationLock
   ],
   imports: [
     BrowserModule
--- src/app/combination-lock/combination-lock.component.ts
+++ src/app/combination-lock/combination-lock.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
     styleUrls: ['./combination-lock.component.css']
 })
 export class CombinationLock {
-    
+    public myArray = ["A", "B", "C", "D", "E"];
+    public currWords = [0, 0, 0, 0, 0];
+
+    rotate(id: number): void {
+        this.currWords[id] = (this.currWords[id] + 1) % this.myArray.length;
+        this.checkPhrase();
+    }
+
+    public correctPhrase = false;
+
+    //unlocks when the phrase D A B D A  is input on the buttons
+    checkPhrase(): void {
+        if (this.currWords[0] === 3 && this.currWords[1] === 0 && this.currWords[2] === 1 && this.currWords[3] === 3 && this.currWords[4] === 0)
+            this.correctPhrase = true;
+        else
+            this.correctPhrase = false;
+    }
 }
