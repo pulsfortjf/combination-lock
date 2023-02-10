28295ad5c86f3a211da449718b071b9d8331b158
Jack Pulsfort <pulsfortjf@appstate.edu>
2023-02-10 09:47:39 -0500

    started combination lock component

---
+++ src/app/combination-lock/combination-lock.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'combination-lock-component',
+    templateUrl: './combination-lock.component.html',
+    styleUrls: ['./combination-lock.component.css']
+})
+export class CombinationLock {
+    
+}
